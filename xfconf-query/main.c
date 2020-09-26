13bf1f22b28d802aa724f9b6e01d4ea8b5ecd70a
dece <shgck@pistache.land>
2020-09-26 16:07:22 +0200

    xion: xfconf-query edits and build system

--- xfconf-query/main.c
+++ xfconf-query/main.c
@@ -285,12 +285,10 @@ main(int argc, char **argv)
         gchar **channels;
         gint i;
 
-        g_print("%s\n", _("Channels:"));
-
         channels = xfconf_list_channels();
         if(G_LIKELY(channels)) {
             for(i = 0; channels[i]; ++i)
-                g_print("  %s\n", channels[i]);
+                g_print("%s\n", channels[i]);
             g_strfreev(channels);
         }
         else
@@ -358,10 +356,12 @@ main(int argc, char **argv)
                 return EXIT_FAILURE;
             }
 
-            if(G_TYPE_PTR_ARRAY != G_VALUE_TYPE(&value))
+            GType value_type = G_VALUE_TYPE(&value);
+            if(G_TYPE_PTR_ARRAY != value_type)
             {
+                const gchar *type_name = g_type_name(value_type);
                 gchar *str_val = _xfconf_string_from_gvalue(&value);
-                g_print("%s\n", str_val ? str_val : _("(unknown)"));
+                g_print("t:%s %s\n", type_name, str_val ? str_val : _("(unknown)"));
                 g_free(str_val);
                 g_value_unset(&value);
             }
@@ -370,8 +370,7 @@ main(int argc, char **argv)
                 GPtrArray *arr = g_value_get_boxed(&value);
                 guint i;
 
-                g_print(_("Value is an array with %d items:"), arr->len);
-                g_print("\n\n");
+                g_print(_("a:%d\n"), arr->len);
 
                 for(i = 0; i < arr->len; ++i)
                 {
@@ -379,8 +378,9 @@ main(int argc, char **argv)
 
                     if(item_value)
                     {
+                        const gchar *type_name = g_type_name(G_VALUE_TYPE(item_value));
                         gchar *str_val = _xfconf_string_from_gvalue(item_value);
-                        g_print("%s\n", str_val ? str_val : _("(unknown)"));
+                        g_print("t:%s %s\n", type_name, str_val ? str_val : _("(unknown)"));
                         g_free(str_val);
                     }
                 }
